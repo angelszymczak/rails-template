f9353b6bbc02f4f1e82c65664e11d1d176aefe98
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-04 19:59:53 -0300

    🟢 email column can not be null

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_180015) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_225505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
