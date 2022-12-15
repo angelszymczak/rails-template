8bba631a949ff2fd1adbfa92518b5bdf0d4682c4
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-15 18:47:34 -0300

    User registration (#8)
    * 🟠 add user registration when credentials are ok
    * 🔴 GET /signup should display the user registration form
    * 🟢 GET /signup shows the user registration form
    * 🔴 POST /signup should handle user data registration
    * 🟢 POST /signup endpoint handles the registration form
    * 🔴 user model should store user registration data
    * 🟢 succesfully POST /signup shows the stored user
    * 🔵 build the form with model helper and secure user input params
    * 🔵 extract user form into partial
    * 🔵 extract user component into partial
    * 🔵 use attributes_for instead of manual build the parameters
    * 🟠 decline user registration when the credentials are empty
    * 🔴 user should be invalid when the email is not present
    * 🟢 user model validates email presence
    * 🔴 email column must not be null
    * 🟢 email column can not be null
    * 🔴 user should be invalid when the password is not present
    * 🟢 user model validates password presence
    * 🔴 password column must not be null
    * 🟢 password column can not be null
    * 🔴 should display the required data
    * 🟢 wrongly POST /signup render errors when credentials are empty
    * 🟠 decline user registration when the credentials are duplicated
    * 🔴 user should be invalid when the email is duplicated
    * 🟢 model validates email uniqueness and the column can not be duplicated
    * 🔵 join user model email validations
    * 🔴 POST /signup should display duplicated data error
    * 🔵 extract registration user errors component into partial
    * 🟠 decline user registration when the email format is not allowed
    * 🔴 user should be invalid when the email is not allowed format
    * 🟢 model validates email format
    * 🟢 POST /signup display email format error
    * 🟠 decline user registration when the password format is not allowed
    * 🔴 user should be invalid when the format password is not accomplished
    * 🟢 model validates password format

---
+++ features/step_definitions/register_user_steps.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+Given('a page for user registration') do
+  visit('/signup')
+  expect(page.find('#user_registration_form')).to be_present
+end
+
+Given('a {string} email and {string} password data') do |guest_email, guest_password|
+  @data = { email: guest_email, password: guest_password }
+end
+
+When('sending user registration form') do
+  within('#user_registration_form') do
+    fill_in('user_email', with: @data[:email])
+    fill_in('user_password', with: @data[:password])
+  end
+  click_button('Register')
+end
+
+Then('user {string} is added') do |guest_email|
+  expect(find('#user').find('#email')).to have_content(guest_email)
+end
+
+Then('display required data error') do
+  expect(find('#user_errors')).to have_content(/Email can't be blank/)
+  expect(find('#user_errors')).to have_content(/Password can't be blank/)
+end
+
+Given('a {string} already registered') do |duplicated_email|
+  FactoryBot.create(:user, email: duplicated_email)
+
+  expect(User.find_by(email: duplicated_email)).to be_present
+end
+
+Then('display duplicated data error') do
+  expect(find('#user_errors')).to have_content(/Email has already been taken/)
+end
+
+Then('display email format error') do
+  expect(find('#user_errors')).to have_content(/Email invalid format error/)
+end
+
+Then('display password format error') do
+  errors = find('#user_errors')
+  expect(errors).to have_content(/Password is too short \(minimum is 8 characters\)/)
+  expect(errors).to have_content(/Password must has at least one uppercase letter/)
+  expect(errors).to have_content(/Password must has at least one digit/)
+  expect(errors).to have_content(/Password must has at least one special character/)
+  expect(errors).to have_content(/Password must has not white spaces/)
+end
