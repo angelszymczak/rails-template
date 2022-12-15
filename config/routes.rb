662fed60071d2908f12a65d28f230212a57ea061
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-15 16:37:18 -0300

    Revert "User registration"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/signup', controller: :registrations, action: :new
-  post '/signup', controller: :registrations, action: :create
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
