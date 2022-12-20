a6864b337c00bd8189b45b3cf992c83243f9b264
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-20 00:18:53 -0300

    🟢 GET /login display the user login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   get '/signup', controller: :registrations, action: :new
   post '/signup', controller: :registrations, action: :create
+
+  get '/login', controller: :sessions, action: :new
+
 end
