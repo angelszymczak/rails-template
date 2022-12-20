71b010b097a9085d757c6825bfe7b8b70fa0692b
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-20 09:09:06 -0300

    🔴 POST /login should log-in user and redirec to root page

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe 'Sessions' do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe 'POST /login' do
+    before { create(:user, user_params) }
+
+    let(:user_params) { attributes_for(:user) }
+
+    it 'returns http FOUND' do
+      post '/login', params: { user: user_params }
+      expect(response).to have_http_status(:found)
+    end
+  end
 end
